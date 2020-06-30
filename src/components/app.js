7da4d3a4e9744ecb7a5a91d7dea4d03582072de3
GabeHD94 <gabehd94@gmail.com>
2020-06-30 14:07:29 -0600

    added getdata handler

--- src/components/app.js
+++ src/components/app.js
@@ -9,6 +9,8 @@ export default class App extends Component {
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleDownload = this.handleDownload.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleGetData = this.handleGetData.bind(this);
   }
 
 handleChange(event) {
@@ -27,12 +29,41 @@ handleDownload() {
   window.url.revokeObjectURL(url)
 }
 
+handleSubmit() {
+  const form = new FormData()
+  form.append("name", this.state.file.name)
+  form.append("type", this.state.file.type)
+  form.append("data", this.state.file)
+
+  fetch("http://127.0.0.1:5000/file/add", {
+    method: "POST",
+    body: form
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error))
+}
+
+handleGetData(){
+  fetch("http://127.0.0.1:5000/file/get/1", { method: "GET" })
+  .then(response => response.blob())
+  .then(data => {
+    const file = new File([data], "Test.jpg", { type: "image/jpeg" })
+    this.setState({ file: file })
+
+  })
+  .catch(error => console.log(error))
+}
+
   render() {
     return (
       <div className='app'>
         <input onChange={this.handleChange} type="file"/>
+        <button onClick={this.handleSubmit}>Submit</button>
+        <hr/>
+        <button onClick={this.handleGetData}>Get File</button>
         <button onClick={this.handleDownload}>Download</button>
       </div>
-    );
+    ); 
   }
 }
